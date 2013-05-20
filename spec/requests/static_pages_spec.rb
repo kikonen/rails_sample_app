8dcdaa536cb6d086710c84b1bcd55e53647ab621
kari <mr.kari.ikonen@gmail.com>
2013-05-20 17:41:07 +0300

    PAGE: Improved layouts - ADD: separate partials for header/footer - ADD: styles - ADD: bootstrap SASS TEST: improved tests - FIX: simplified test spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,34 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe 'static pages' do
-  	let(:base_title) {'Sample App'}
+describe "Static Pages" do
+  subject { page }
 
-  	describe 'home' do
-  	  it 'should have content "sample app"' do
-  	  	visit '/static_pages/home'
-  	  	page.should have_selector('h1', text: 'Sample App')
-  	  end
+	describe 'home' do
+    before(:each) { visit root_path }
 
-  	  it 'should have right title' do
-  	  	visit '/static_pages/home'
-  	  	page.should have_selector('title', text: "#{base_title}")
-  	  end
+	  it { should have_selector('h1', text: 'Sample App') }
+	  it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: " | Home") }
+	end
 
-      it 'should not have extension title' do
-        visit '/static_pages/home'
-        page.should_not have_selector('title', text: " | Home")
-      end
-  	end
+	describe 'help' do
+    before(:each) { visit help_path }
 
-  	describe 'help' do
-  	  it 'should have content "help"' do
-  	  	visit '/static_pages/help'
-  	  	page.should have_selector('h1', text: 'Help')
-  	  end
+	  it { should have_selector('h1', text: 'Help') }
+	  it { should have_selector('title', text: full_title('Help')) }
+	end
 
-  	  it 'should have right title' do
-  	  	visit '/static_pages/help'
-  	  	page.should have_selector('title', text: "#{base_title} | Help")
-  	  end
-  	end
+	describe 'about' do
+    before(:each) { visit about_path }
 
-  	describe 'about' do
-  	  it 'should have content "about"' do
-  	  	visit '/static_pages/about'
-  	  	page.should have_selector('h1', text: 'About Us')
-  	  end
+	  it { should have_selector('h1', text: 'About Us') }
+	  it { should have_selector('title', text: full_title('About')) }
+	end
 
-  	  it 'should have right title' do
-  	  	visit '/static_pages/about'
-  	  	page.should have_selector('title', text: "#{base_title} | About")
-  	  end
-  	end
+	describe 'contact' do
+    before(:each) { visit contact_path }
 
-  	describe 'contact' do
-  	  it 'should have content "contact"' do
-  	  	visit '/static_pages/contact'
-  	  	page.should have_selector('h1', text: 'Contact')
-  	  end
-
-  	  it 'should have right title' do
-  	  	visit '/static_pages/contact'
-  	  	page.should have_selector('title', text: "#{base_title} | Contact")
-  	  end
-  	end
-  end
-
+	  it { should have_selector('h1', text: 'Contact') }
+	  it { should have_selector('title', text: full_title('Contact')) }
+	end
 end
---
+++ spec/support/utlities.rb
@@ -0,0 +1,10 @@
+
+# get full title
+def full_title(page_title)
+  base_title = 'Sample App'
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
