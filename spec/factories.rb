5d4f4523286a2a9a6d425d2971fdb37ad530ec65
kari <mr.kari.ikonen@gmail.com>
2013-05-21 01:10:39 +0300

    NEW: - Signup of new user - Applied FactoryGirl into use - error messages display

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+FactoryGirl.define do
+  factory :user do
+    name     'Test User'
+    email    'test@test.com'
+    password 'foobar'
+    password_confirmation 'foobar'
+  end 
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,44 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe 'UserPages' do
   subject { page }
 
-  describe "signup" do
+  describe 'signup' do
     before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up')}
     it { should have_selector('title', text: full_title('Sign up'))}
+  end
+
+  describe 'profile' do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name)}
+    it { should have_selector('title', text: user.name)}
+  end
+
+  describe 'signup' do
+    before { visit signup_path }  
+    let(:submit) { "Create My Account" }
+
+    describe 'with invalid data' do
+      it 'should not create user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'with valid data' do
+      before do
+        fill_in 'Name',         with: 'Example User'
+        fill_in 'Email',        with: 'user@example.com'
+        fill_in 'Password',     with: 'foobar'
+        fill_in 'Confirmation', with: 'foobar'
+      end
+
+      it 'should create user' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 end
