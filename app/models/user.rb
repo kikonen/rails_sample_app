40b0d8ef47c1e6ef88cdf0aced9358a7cf9253c3
kari <mr.kari.ikonen@gmail.com>
2013-05-21 01:41:33 +0300

    NEW: - options for gravatar 	* size - tests 	* error validation 	* sign in validation FIX: - password error rendering - Improved flash rendering logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,6 @@ class User < ActiveRecord::Base
       uniqueness: { case_sensitive: false }
 
   validates :password, 
-      presence: true, 
       length: { minimum: 6 }
 
   validates :password_confirmation, 
