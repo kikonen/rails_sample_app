4349b658429c493d0e188f0e70569b8cc9e8a7a1
kari <mr.kari.ikonen@gmail.com>
2013-05-20 22:31:37 +0300

    MODEL: secure password for user - ADD: secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,32 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: 'Example User', email: 'user@example.com') }
+  before { @user = User.new(
+      name: 'Example User', 
+      email: 'user@example.com',
+      password: 'foobar',
+      password_confirmation: 'foobar') }
+
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -83,4 +94,39 @@ describe User do
     it { should_not be_valid}
   end
 
+  describe 'password missing' do
+    before { @user.password = @user.password_confirmation = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'password confirm mismatch' do
+    before { @user.password_confirmation = 'mismatch' }
+    it { should_not be_valid }
+  end
+
+  describe 'password confirm nil' do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe 'too short password' do 
+    before { @user.password = @user.password_confirmation = 'az' }
+    it { should be_invalid }
+  end
+
+  describe 'authenticate result' do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe 'valid password' do 
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe 'invalid password' do 
+      let(:user_for_invalid_password) { found_user.authenticate('invalid pwd') }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end    
+  end 
 end
